7305269f8abee2f29019678bb7d9d5a8dd3e6264
Tigre <cuji309@gmail.com>
2026-06-03 23:50:06 -0500

    feat: implementacion componente app-profile y navegación dinámica desde las cards

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
 import { AppHero } from './components/app-hero/app-hero';
+import { AppProfile } from './components/app-profile/app-profile';
 
 export const routes: Routes = [
     {path: '', component: AppHero},
+    {path: 'programador/:id', component: AppProfile},
     {path: '**', redirectTo: ''},
 ];
     
